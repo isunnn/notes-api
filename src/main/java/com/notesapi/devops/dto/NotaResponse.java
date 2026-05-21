50589f7bf5d982f9f21ac32c2b47ab543c099c0d
isunnn <nico.jimenezo@duocuc.cl>
2026-05-21 07:29:36 -0400

    ci: Se añade SonarCloud + despliegue simulado con Docker compose

--- src/main/java/com/notesapi/devops/dto/NotaResponse.java
+++ src/main/java/com/notesapi/devops/dto/NotaResponse.java
@@ -2,12 +2,12 @@ package com.notesapi.devops.dto;
 
 import java.time.Instant;
 
-public record NotaResponse (
-    Long id,
-    String nombreNota,
-    Instant fechaCreacion
+public record NotaResponse(
+        Long id,
+        String nombreNota,
+        Instant fechaCreacion
 
 )
 
-
-{}
+{
+}
